1b73a9406bbe4d8040ae36e191fe8c27f0a2392b
Jivago Alves <jivagoalves@gmail.com>
2012-04-18 20:16:53 -0300

    POST create for activity

--- app/controllers/activities_controller.rb
+++ app/controllers/activities_controller.rb
@@ -2,4 +2,13 @@ class ActivitiesController < ApplicationController
   def new
     @activity = Activity.new
   end
+
+  def create
+    activity = Activity.new(params[:activity])
+    if activity.save
+      redirect_to new_activity_path
+    else
+      render 'new'
+    end
+  end
 end
--- spec/controllers/activities_controller_spec.rb
+++ spec/controllers/activities_controller_spec.rb
@@ -16,4 +16,44 @@ describe ActivitiesController do
       assigns(:activity).should == "a new activity"
     end
   end
+
+  describe '#create' do
+    let(:activity) { mock_model('Activity').as_null_object }
+
+    before :each do
+      Activity.stub(:new).and_return(activity)
+    end
+
+    it 'should create a new activity from user input' do
+      Activity.should_receive(:new).with('description' => 'my activity')
+      post :create, :activity => {'description' => 'my activity'}
+    end
+
+    it 'should ask to save the activity' do
+      activity.should_receive(:save)
+      post :create, :activity => {'description' => 'my activity'}
+    end
+
+    context 'when the activity saves successfully' do
+      before :each do
+        activity.stub(:save).and_return(true)
+      end
+
+      it 'should redirect to new template' do
+        post :create, :activity => {'description' => 'my activity'}
+        response.should redirect_to(new_activity_path)
+      end
+    end
+
+    context 'when the activity creation fails' do
+      before :each do
+        activity.stub(:save).and_return(false)
+      end
+
+      it 'should render the new template' do
+        post :create, :activity => {}
+        response.should render_template('new')
+      end
+    end
+  end
 end
