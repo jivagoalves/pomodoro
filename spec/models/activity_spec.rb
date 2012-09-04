fe91f99b0658c94d7d580e7435293ddbc3310a0e
Jivago Alves <jivagoalves@gmail.com>
2012-09-03 23:01:04 -0300

    Rewrite spec for activity model

--- spec/models/activity_spec.rb
+++ spec/models/activity_spec.rb
@@ -1,59 +1,21 @@
 require 'spec_helper'
 
 describe Activity do
-  let(:activity) { Activity.new }
+  it { should_not be_done }
+  its(:description) { should == "" }
 
-  it 'should have a property: description' do
-    activity.description = "my description"
-    activity.description.should == "my description"
-  end
-
-  it 'should have a property: done' do
-    activity.done = true
-    activity.done.should == true
-  end
-
-  describe '#new' do
-    it 'should initialize description with ""' do
-      activity.description.should == ""
-    end
-
-    it 'should initialize done with false' do
-      activity.done.should == false
-    end
-  end
-
-  describe '#all' do
-    before :each do
-      Activity.all.each {|act| act.destroy}
-      @activities = [
-        '2011-05-11',
-        '2011-12-11',
-        '2012-05-11'
-      ].map do |date|
-        act = Activity.new
-        act.created_at = date
-        act.save!
-        act
+  describe "default scope" do
+    before do
+      2.times do |i|
+        Activity.new(description: i) do |a|
+          a.created_at = (i + 1).second.ago
+          a.save!
+        end
       end
     end
 
-    it 'should order by creation date' do
-      Activity.all.should == @activities.reverse
-    end
-  end
-
-  context 'on loading an activity from the database' do
-    before :each do
-      activity.description = "my description"
-      activity.done = true
-      activity.save
-    end
-
-    it 'should not overwrite with default values' do
-      activity_found = Activity.find_by_id(activity.id)
-      activity_found.description.should == "my description"
-      activity_found.done.should == true
+    it "orders by creation date desc" do
+      Activity.all.map(&:description).should == ['0', '1']
     end
   end
 end
