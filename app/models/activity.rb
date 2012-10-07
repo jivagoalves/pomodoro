b59d2e5f8b35743ee07ddeba74038e20ffbd2eb3
Jivago Alves <jivagoalves@gmail.com>
2012-10-07 16:53:00 -0300

    Organize Rails model

--- app/models/activity.rb
+++ app/models/activity.rb
@@ -1,8 +1,9 @@
 class Activity < ActiveRecord::Base
   attr_accessible :description, :done
-  default_scope :order => 'created_at DESC'
 
   has_many :spent_times, dependent: :destroy
+
+  default_scope :order => 'created_at DESC'
 
   after_initialize do |activity|
     activity.description ||= ""
