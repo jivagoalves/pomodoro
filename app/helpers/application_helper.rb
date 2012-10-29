0cafe3a48e3a800680c2ca73412cb8796e321f5b
Jivago Alves <jivagoalves@gmail.com>
2012-10-28 23:12:01 -0300

    Add helper to convert seconds

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,2 +1,11 @@
 module ApplicationHelper
+  def seconds_in_hh_mm_ss(seconds)
+    mm, ss = seconds.divmod(60)
+    hh, mm = mm.divmod(60)
+    dd, hh = hh.divmod(24)
+    ss = ss < 10 ? "0#{ss}" : ss
+    mm = mm < 10 ? "0#{mm}" : mm
+    hh = hh < 10 ? "0#{hh}" : hh
+    "#{hh}:#{mm}:#{ss}"
+  end
 end
