5bdccf998a91d79193774665c14d9d2a48c05a05
Jivago Alves <jivagoalves@gmail.com>
2012-10-30 23:04:19 -0300

    Change time zone to Brasilia

--- config/application.rb
+++ config/application.rb
@@ -34,6 +34,7 @@ module Pomodoro
     # Set Time.zone default to the specified zone and make Active Record auto-convert to this zone.
     # Run "rake -D time" for a list of tasks for finding time zone names. Default is UTC.
     # config.time_zone = 'Central Time (US & Canada)'
+    config.time_zone = 'Brasilia'
 
     # The default locale is :en and all translations from config/locales/*.rb,yml are auto loaded.
     # config.i18n.load_path += Dir[Rails.root.join('my', 'locales', '*.{rb,yml}').to_s]
@@ -49,6 +50,8 @@ module Pomodoro
     # This is necessary if your schema can't be completely dumped by the schema dumper,
     # like if you have constraints or database-specific column types
     # config.active_record.schema_format = :sql
+
+    config.active_record.default_timezone = 'Brasilia'
 
     # Enforce whitelist mode for mass assignment.
     # This will create an empty whitelist of attributes available for mass-assignment for all models
